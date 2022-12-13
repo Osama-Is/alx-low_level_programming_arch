75a1a9af16dce15af101c2731d42a3e2e2776908
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-13 08:15:04 +0000

    Solve task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints string to the standard output
+ *
+ * Return: 0 if executed successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
