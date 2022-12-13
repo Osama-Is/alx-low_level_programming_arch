0dc4c6dceab020b6d1b58b8892f29f7a2309742f
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-13 07:46:25 +0000

    Solve task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print string to standart output
+ * main - prints string to standart output
+ *
+ * Return: 0 if executed successfully
  */
 int main(void)
 {
 	char str[] = "Programming is like building a multilingual puzzle";
-	puts(str);
-	return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
