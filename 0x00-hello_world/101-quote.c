d27a9509022e75f16be5d84fcb0c07d014098a82
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-13 16:37:02 +0000

    Solve task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * main - Print a string to strerr
+ *
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(2, str, strlen(str);
+}
