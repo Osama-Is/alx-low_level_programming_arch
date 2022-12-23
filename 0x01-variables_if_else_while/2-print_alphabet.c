a5f6351f4b5ce9aa1974564129fd9662b727d450
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-23 23:30:47 +0000

    Solve task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char i = 97;
+	while (i < 122)
+	{
+		putchar(i);
+		i++;
+	}
+}
