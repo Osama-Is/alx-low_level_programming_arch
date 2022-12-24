7d62aab64582a8c2834d97783b4e01f6b1ff3133
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-24 01:03:04 +0000

    Solve task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	char i = 122;
+
+	while (i >= 97)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
