c027404ccbddd409b47127ce5ffbab2429b1469f
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-24 01:29:15 +0000

    Solve task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	char i = 97;
+
+	while (i <= 103)
+	{
+		putchar('\n');
+		i++;
+	}
+
+	i = 48;
+	
+	while (i <= 57)
+	{
+		putchar('\n');
+	}
+
+	putchar('\n);
+
+	return (0);
+}
