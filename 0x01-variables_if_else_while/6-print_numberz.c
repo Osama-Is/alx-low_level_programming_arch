4dec7cd960fdf24b3c0ecf88b7bec05970853251
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-24 00:55:51 +0000

    Solve task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all digit numbers of base 10
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	char i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
