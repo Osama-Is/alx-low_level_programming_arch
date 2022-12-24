9dbeeb4d49854876399e0e5c28b116c1818a75d0
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-24 07:40:13 +0000

    Solve task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of three digits
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	short x = 48;
+	short y = 49;
+	short z = 50;
+	
+	while (x <= 55)
+	{
+		while (y <= 56)
+		{
+			while (z <= 57)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+
+				if (x == 55 && y == 56 && z == 57)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				z++;
+			}
+			y++;
+			z = y + 1;
+		}
+		x++;
+		y = x + 1;
+	}
+	return (0);
+}
