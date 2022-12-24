f5975f7924fc1c49ce5b5ff58435ae73d1d61065
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-24 00:44:18 +0000

    Solve task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all digit numbers of base 10
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	char i = 0;
+
+	while (i <= 9)
+	{
+		printf('%d', i);
+	}
+	putchar('\n');
+}
