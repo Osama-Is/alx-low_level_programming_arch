f79dbd6923c9da7446f627652b256c70066abfbd
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-24 00:16:06 +0000

    Solve task 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all letters in lowercase except q & e
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char i = 97;
+	char lttre = 'e';
+	char lttrq = 'q';
+
+	while (i <= 122)
+	{
+		if (i != 'e' || i != 'q') 
+		{
+			putchar(i);
+			i++;
+		}
+	}
+
+	return (0);
+}
