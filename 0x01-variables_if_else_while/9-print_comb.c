117e11df20702c1a0fb636fc1b2751d287de3388
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-24 01:51:52 +0000

    Solve task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	short i = 48;
+
+	while (i < 57)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
