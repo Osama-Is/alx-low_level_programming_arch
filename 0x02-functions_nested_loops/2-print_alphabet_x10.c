689ed1d9fb31e16809e3014cb2f3d0b1d7ce0730
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-30 03:54:04 +0000

    Solve task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,17 @@
 void print_alphabet_x10(void)
 {
 	char i = 0;
+	char a;
 
 	while (i < 10)
 	{
-		print_alphabet();
+		a = 97;
+		while (a <= 122)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
