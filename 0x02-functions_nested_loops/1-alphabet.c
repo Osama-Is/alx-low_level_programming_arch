f72f6de306d06c2f62aac8c5621fa08f3d6a9b5a
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-30 02:04:23 +0000

    Solve task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char i = 97;
+	
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+
+	return;
+}
