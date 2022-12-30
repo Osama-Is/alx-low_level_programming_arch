6efc64867415b075930f19e7fe83b9c577d5aca7
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-30 06:50:02 +0000

    Solve task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @i: The integer
+ *
+ * Return: The absolute value
+ */
+int _abs(int i)
+{
+	signed a = i;
+	return (a);
+}
