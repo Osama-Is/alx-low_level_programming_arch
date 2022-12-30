66486994f719f42a7f1d9aa383b4c5e58203829e
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2022-12-30 03:47:36 +0000

    Solve task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h>"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char i = 0;
+
+	while (i <= 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}
