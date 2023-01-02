cefdb950f7da875beadcbd69595fed1df3578c67
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-02 12:31:19 +0000

    Solve task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	short hrstn = 48;
+	short hrson = 48;
+	short mnstn = 48;
+	short mnson = 48;
+
+	while (x1 < 51)
+	{
+		while (x2 <= 57)
+		{
+			while (y1 < 54)
+			{
+				while (y2 <= 57)
+				{
+					_putchar(hrstn);
+					_putchar(hrson);
+					_putchar(':');
+					_putchar(mnstn);
+					_putchar(mnson);
+					_putchar('\n');
+					if (hrsrn == 50 &&\
+						hrson == 51 &&\
+						mnstn == 53 &&\
+						mnson == 57)
+					{
+					    y2 = 100;
+					    y1 = 100;
+					    x2 = 100;
+					    x2 = 100;
+					}
+					y2++;
+				}
+				y2 = 48;
+				y1++;
+			}
+			y1 = 48;
+			x2++;
+		}
+		x2 = 48;
+		x1++;
+	}
+}
