9ffe5cecc676c471b6f88f2e53da673b799c503f
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-04 17:37:53 +0000

    Solve task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+			int value = i * j;
+			
+			if (value < 10)
+			{
+				_putchar(' ');
+				_putchar('0', value % 10);
+			}
+			else
+			{
+				_putchar('0' + (value / 10));
+				_putchar('0' + (value % 10));
+			}
+		}
+		_putchar('$');
+	}
+}
