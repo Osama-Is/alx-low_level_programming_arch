99dbdb50f24894175ad5bbf0d0fad6e2ef85a2ca
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-05 00:18:57 +0000

    Solve task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,14 @@
 
 /**
  * add - Adds two integers and returns the result
+ * @fnum: first operand number
+ * @snum: second operand number
  *
  * Return: Sum
  */
 int add(int fnum, int snum)
 {
 	int sum = fnum + snum;
-	
+
 	return (sum);
 }
