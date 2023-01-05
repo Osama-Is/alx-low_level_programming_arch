bcec181c10c82d90d41c1208d30a727956ce8f3a
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-05 03:01:22 +0000

    Solve task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,10 +3,12 @@
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number from which to start printing
+ *
+ * Desription: n < 98 || n > 98
  */
 void print_to_98(int n)
 {
-	for (; n <= 98; n++)
+	while ((n <= 98) || (n >= 98))
 	{
 		if (n == 98)
 		{
@@ -17,5 +19,6 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
+		n += 98 - n;
 	}
 }
