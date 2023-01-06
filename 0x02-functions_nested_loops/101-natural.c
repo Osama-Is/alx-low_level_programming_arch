ea13d745dec92f32b8ab3805bb8cacaba5ba499e
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-06 15:49:36 +0000

    Solve task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * main - Prints the sum of all the multiples of 3 or 5 below 1024
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	int sum;
+	int summul3;
+	int summul5;
+	int lmul3 = 1024 / 3;
+	int lmul5 = 1024 / 5;
+	int ntrm3 = lmul3 / 3;
+	int ntrm5 = lmul5 / 5;
+
+	summul3 = 3 * ((1 + ntrm3) * (ntrm3 / 2));
+	summul5 = 5 * ((1 + ntrm5) * (ntrm5 / 2));
+
+	printf("%d", summul3 + summul5);
+
+	return (0);
+}
