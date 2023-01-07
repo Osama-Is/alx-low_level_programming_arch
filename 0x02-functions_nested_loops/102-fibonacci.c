356ffcc0b987b88d4369814102d77984a42f3e5d
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-07 06:53:20 +0000

    Solve task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	int fpmmb = 1;
+	int spmmb = 2;
+	int i;
+
+	for (i = 2; i <= 50; i++)
+	{
+		int curr = fpmmb + spmmb;
+		printf("%d, ", curr);
+
+		fpmmb = spmmb;
+		spmmb = curr;
+	}
+	putchar('\n');
+
+	return (0);
+}
