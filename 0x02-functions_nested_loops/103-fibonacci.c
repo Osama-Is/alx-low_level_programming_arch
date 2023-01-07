24821a68446951a56d2416e0f58844b83929edb1
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-07 08:04:38 +0000

    Solve task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of the even-valued terms of Fibonacci sequence
+ *
+ * Description: The values of terms for Fibonacci sequence 
+ * do not exceed 4,000,000.
+ * Return: Zero
+ */
+int main(void)
+{
+	long int a = 1, b = 2, sum;
+	int i;
+
+	while ((a+b) <= 4000000)
+	{
+		long int c = a + b;
+
+		if ((c % 2) == 0)
+		{
+			sum += c;
+		}
+		a = b;
+		b = c;
+	}
+	printf("%ld\n", sum);
+
+	return (0);
+}
