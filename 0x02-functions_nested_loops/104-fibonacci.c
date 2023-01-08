b5fa3f2eb9bb7bc4a5b54d8d2bd8a26dc5926d38
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-08 11:03:42 +0000

    Solve task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2
+ *
+ * Description: not allowed to use any other library (You can’t use GMP etc…).
+ * Not allowed to use long long, malloc, pointers, arrays/tables, or structures
+ * Not allowed to hard code any Fibonacci number (except for 1 and 2).
+ * Return: Zero
+ */
+int main(void)
+{
+	long int a1 = 0, a2 = 1;
+	long int b1 = 0, b2 = 2;
+	int i;
+
+	printf("%ld, %ld, ", a2, b2);
+
+	for (i = 3; i <= 98; i++)
+	{
+		long int c1 = (a1 + b1) / 1000000000;
+		long int c2 = (a2 + b2) % 1000000000;
+
+		printf("%ld%ld", c1, c2);
+		if (i != 98)
+		{
+			printf(", ");
+		}
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
+	}
+	putchar('\n');
+
+	return (0);
+}
