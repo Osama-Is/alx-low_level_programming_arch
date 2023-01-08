9907c1eb60976bc1fe4473d727e2900dd31bfd65
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-08 12:17:03 +0000

    Solve task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("%ld, %ld, ", a2, b2);
 	for (i = 3; i <= 98; i++)
 	{
-		long int a1, b1;
+		long int a1 = 0, b1 = 0;
 		long int c1 = (a2 + b2) / 1000000000;
 		long int c2 = (a2 + b2) % 1000000000;
 
