9d7cdea5f20703e28fbbf34334019c5059475b0e
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-14 16:34:13 +0000

    Solve task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,6 +3,11 @@
 /**
  * print_line - Draws a straight line in the terminal
  * @n: Width of the straight line
+ *
+ * Description:
+ * 	- Only '_putchar' function is allowed.
+ * 	- The line should end with a \n.
+ * 	- If n is 0 or less, the function should only print \n.
  */
 void print_line(int n)
 {
