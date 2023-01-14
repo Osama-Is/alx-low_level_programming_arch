966ae2dab8de6984918c8d9859f4b14d4c535563
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-14 14:28:13 +0000

    Solve task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers, from 0 to 9
- *
- * Decription: It doesn't print 2 & 4
+ * print_most_numbers - Prints the numbers, from 0 to 9, except 2 & 4
  */
 void print_most_numbers(void)
 {
