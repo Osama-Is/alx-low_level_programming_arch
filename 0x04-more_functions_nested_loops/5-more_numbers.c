f37a206f7ad5761464c2baa4e4ce7e934d39ab7a
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-14 14:38:55 +0000

    Solve task 4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i, y;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			int fd, sd;
+
+			fd = y % 10;
+			_putchar(fd + '0');
+
+			if (y >= 10)
+			{
+				sd = y / 10;
+				_putchar(sd + '0');
+			}
+		}
+		_putchar('0');
+	}
+}
