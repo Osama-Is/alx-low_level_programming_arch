94d4720b6d9593497e5dae14db713a322d7d5eeb
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-14 16:35:03 +0000

    Solve task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,9 +5,9 @@
  * @n: Width of the straight line
  *
  * Description:
- * 	- Only '_putchar' function is allowed.
- * 	- The line should end with a \n.
- * 	- If n is 0 or less, the function should only print \n.
+ * - Only '_putchar' function is allowed.
+ * - The line should end with a \n.
+ * - If n is 0 or less, the function should only print \n.
  */
 void print_line(int n)
 {
