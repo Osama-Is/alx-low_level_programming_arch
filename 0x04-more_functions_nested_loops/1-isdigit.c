42be4c50d3a4669bc28e4f396d97e59d55e0b6da
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-14 12:37:17 +0000

    Solve task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - Checks for a digit
+ * _isdigit - Checks for a digit between '0' and '9'
  * @c: The character to be checked
  *
  * Returns: '1' if True, '0' otherwise
