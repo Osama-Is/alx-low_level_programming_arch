42684e96452bc0f21e2e8ff93d2283de70f562c4
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-17 23:23:45 +0000

    Solve task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,14 +12,15 @@
  */
 void print_number(int n)
 {
+
+	if ((n % 10) != 0)
+	{
+		print_number(n / 10);
+	}
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+	_putchar((n % 10) + '0');
 }
