949fe29daeb83b753bd01c92e3a1b31917f676fc
Osama Gamal <37333584+Osama-Is@users.noreply.github.com>
2023-01-17 22:38:51 +0000

    Solve task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ *
+ * Decription:
+ * - Only '_putchar' function is allowed to print
+ * - Not allowed to use long
+ * - Not allowed to use array pointers
+ * - Not allowed to hard codde special values
+ */
+void print_number(int n)
+{
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10);
+}
